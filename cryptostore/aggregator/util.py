8bbb03910bbaac3cd3ede8392710205f9f33e144
Bryant Moscon <bmoscon@gmail.com>
2019-05-21 19:25:20 -0400

    add util file

---
+++ cryptostore/aggregator/util.py
@@ -0,0 +1,31 @@
+'''
+Copyright (C) 2018-2019  Bryant Moscon - bmoscon@gmail.com
+
+Please see the LICENSE file for the terms and conditions
+associated with this software.
+'''
+from cryptofeed. defines import BID, ASK
+
+
+def book_flatten(book: dict, timestamp: float, delta: str) -> dict:
+    """
+    takes book and returns a list of dict, where each element in the list
+    is a dictionary with a single row of book data.
+
+    eg.
+    L2:
+    [{'side': str, 'price': float, 'size': float, 'timestamp': float}, {...}, ...]
+
+    L3:
+    [{'side': str, 'price': float, 'size': float, 'timestamp': float, 'order_id': str}, {...}, ...]
+    """
+    ret = []
+    for side in (BID, ASK):
+        for price, data in book[side].items():
+            if isinstance(data, dict):
+                # L3 book
+                for order_id, size in data.items():
+                    ret.append({'side': side, 'price': price, 'size': size, 'order_id': order_id, 'timestamp': timestamp, 'delta': delta})
+            else:
+                ret.append({'side': side, 'price': price, 'size': data, 'timestamp': timestamp, 'delta': delta})
+    return ret
