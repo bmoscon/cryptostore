16166693f8de37852c6b6384459d7a9cccaf0150
Bryant Moscon <bmoscon@gmail.com>
2019-08-23 09:45:11 -0400

    static analysis

--- cryptostore/data/store.py
+++ cryptostore/data/store.py
@@ -8,7 +8,7 @@ class Store:
     def write(self, exchange: str, data_type: str, pair: str, timestamp: float):
         raise NotImplementedError
 
-    def aggregate(self, data: dict):
+    def aggregate(self, data: dict, transform=lambda x: x):
         raise NotImplementedError
 
     def get_start_date(self, exchange: str, data_type:str, pair: str) -> float:
