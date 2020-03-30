548bb310d89e1f5ef1b35a8e9fe4bcc2a876f9b3
Ryan Shtirmer <rshtirmer@gmail.com>
2020-03-30 14:34:08 -0400

    Update util.py

--- cryptostore/util.py
+++ cryptostore/util.py
@@ -4,13 +4,12 @@ Copyright (C) 2018-2020  Bryant Moscon - bmoscon@gmail.com
 Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
-from datetime import timedelta, datetime as dt
+from datetime import timedelta, timezone, datetime as dt
 
 
 def get_time_interval(ts: float, interval: str, multiplier=1):
     timestamp = dt.utcfromtimestamp(ts)
     start_interval, end_interval = None, None
-    
     if interval == 'M':
         end_interval = dt(timestamp.year, timestamp.month, timestamp.day, timestamp.hour, timestamp.minute)
         start_interval = end_interval - timedelta(minutes=multiplier)
@@ -20,9 +19,7 @@ def get_time_interval(ts: float, interval: str, multiplier=1):
     elif interval == 'D':
         end_interval = dt(timestamp.year, timestamp.month, timestamp.day)
         start_interval = end_interval - timedelta(days=multiplier)
-        
     if start_interval and end_interval:
-        start_interval = start_interval.timestamp() * 1000
-        end_interval = end_interval.timestamp() * 1000
-        
+        start_interval = int(start_interval.replace(tzinfo=timezone.utc).timestamp()) * 1000
+        end_interval = int(end_interval.replace(tzinfo=timezone.utc).timestamp()) * 1000
     return start_interval, end_interval
