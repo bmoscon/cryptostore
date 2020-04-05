fc34c50d429cac9e2e328549809cddd65763c3b8
Bryant Moscon <bmoscon@gmail.com>
2020-04-05 16:29:39 -0400

    WIP new missing trades code to replace backfill

--- cryptostore/plugin/trades.py
+++ cryptostore/plugin/trades.py
@@ -4,10 +4,6 @@ Copyright (C) 2018-2020  Bryant Moscon - bmoscon@gmail.com
 Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
-
-##### DEPRECATED #####
-
-
 import time
 import logging
 from pandas import Timestamp, Timedelta
@@ -20,13 +16,12 @@ from cryptofeed.rest import Rest
 from cryptostore.config import Config
 from cryptostore.plugin.plugin import Plugin
 from cryptostore.data.storage import Storage
-from cryptostore.exceptions import InconsistentStorage
 
 
 LOG = logging.getLogger('cryptostore')
 
 
-class Backfill(Plugin):
+class MissingTrades(Plugin):
     def __init__(self, config):
         super().__init__(config)
         self.config = Config(config)
@@ -36,36 +31,23 @@ class Backfill(Plugin):
     def _worker(self, exchange):
         r = Rest()
         storage = Storage(self.config)
-        for pair in self.config.backfill[exchange]:
+        for pair in self.config.missing_trades[exchange]:
             try:
-                start = self.config.backfill[exchange][pair].start
-
-                while True:
-                    end = storage.get_start_date(exchange, 'trades', pair)
-
-                    if all(e for e in end):
-                        break
-                    time.sleep(10)
-                ends = list(map(lambda x: Timestamp(x, unit='s') - Timedelta(microseconds=1), end))
-
-                if any(e <= Timestamp(start) for e in ends):
-                    LOG.info("Data in storage is earlier than backfill start date for %s - %s", exchange, pair)
-                    continue
+                start = self.config.missing_trades[exchange][pair].start
+                lu = self.config.missing_trades
+                if lu and exchange in lu and pair in lu[exchange]:
+                    start = lu[exchange][pair]
 
                 LOG.info("Backfill - Starting for %s - %s for range %s - %s", exchange, pair, start, str(max(ends)))
 
-                # Backfill from end date to start date, 1 day at a time, in reverse order (from end -> start)
-                end = max(ends)
                 start = Timestamp(start)
-                while start < end:
-                    seg_start = end.replace(hour=0, minute=0, second=0, microsecond=0, nanosecond=0)
-                    if start > seg_start:
-                        seg_start = start
-                    LOG.info("Backfill - Reading %s to %s for %s - %s", seg_start, end, exchange, pair)
+                while start < Timestamp.now():
+                    end = (start + Timedelta(1, unit='d')).replace(hour=0, minute=0, second=0, microsecond=0, nanosecond=0) - Timedelta(1, unit='ns')
+                    LOG.info("Backfill - Reading %s to %s for %s - %s", start, end, exchange, pair)
 
                     trades = []
                     try:
-                        for t in r[exchange].trades(pair, str(seg_start), str(end)):
+                        for t in r[exchange].trades(pair, str(start), str(end)):
                             trades.extend(t)
                     except Exception:
                         LOG.warning("Backfill - encountered error backfilling %s - %s, trying again...", exchange, pair, exc_info=True)
@@ -73,7 +55,7 @@ class Backfill(Plugin):
                         continue
 
                     if not trades:
-                        end = seg_start - Timedelta(nanoseconds=1)
+                        start = end + Timedelta(1, unit='ns')
                         continue
 
                     for trade in trades:
