c4ffed9432488f0f06c25fac09dcc5e2899073ad
xiandong79 <xqiad@connect.ust.hk>
2020-04-15 14:36:58 +0800

    Fix the unclear parquet name in S3.

--- cryptostore/data/parquet.py
+++ cryptostore/data/parquet.py
@@ -66,7 +66,7 @@ class Parquet(Store):
 
         if self._write:
             for func, bucket, prefix, kwargs in zip(self._write, self.bucket, self.prefix, self.kwargs):
-                path = f'{exchange}/{data_type}/{pair}/{int(timestamp)}.parquet'
+                path = f'{exchange}/{data_type}/{pair}/{exchange}-{data_type}-{pair}-{int(timestamp)}.parquet'
                 if prefix:
                     path = f"{prefix}/{path}"
                 func(bucket, path, file_name, **kwargs)
