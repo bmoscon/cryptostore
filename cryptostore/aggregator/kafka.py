4f3d610f98c925ee953e7f3344145a2271dbdcb8
Pierre JUILLARD <pierre.juillard@gmail.com>
2020-10-17 07:57:21 +0200

    Connected COINGECKO feed.

--- cryptostore/aggregator/kafka.py
+++ cryptostore/aggregator/kafka.py
@@ -7,7 +7,7 @@ associated with this software.
 import json
 import logging
 
-from cryptofeed.defines import L2_BOOK, L3_BOOK, TRADES, TICKER, FUNDING, OPEN_INTEREST
+from cryptofeed.defines import L2_BOOK, L3_BOOK, TRADES, TICKER, FUNDING, OPEN_INTEREST, PROFILE
 
 from cryptostore.engines import StorageEngines
 from cryptostore.aggregator.cache import Cache
@@ -81,7 +81,7 @@ class Kafka(Cache):
             if dtype in {L2_BOOK, L3_BOOK}:
                 update = book_flatten(update, update['timestamp'], update['delta'])
                 ret.extend(update)
-            if dtype in {TRADES, TICKER, FUNDING, OPEN_INTEREST}:
+            if dtype in {TRADES, TICKER, FUNDING, OPEN_INTEREST, PROFILE}:
                 ret.append(update)
 
         return ret
