31cefb0ecf10a7bcd6f162538479522edccd4851
james <atjameslee@gmail.com>
2021-07-27 19:58:05 +0700

    accept exchange feed options in config (#190)
    * accept exchange feed options in config
    
    * fixed indent
    
    * Fixed config
    
    Co-authored-by: James Lee <lee46james@gmail.com>

--- cryptostore/collector.py
+++ cryptostore/collector.py
@@ -4,13 +4,14 @@ Copyright (C) 2018-2021  Bryant Moscon - bmoscon@gmail.com
 Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
+import inspect
+import logging
 import os
 from multiprocessing import Process
-import logging
 
 from cryptofeed import FeedHandler
 from cryptofeed.defines import TRADES, L2_BOOK, L3_BOOK, MARKET_INFO, BOOK_DELTA, TICKER, FUNDING, OPEN_INTEREST, LIQUIDATIONS, CANDLES
-
+from cryptofeed.exchanges import EXCHANGE_MAP
 
 LOG = logging.getLogger('cryptostore')
 
@@ -23,28 +24,47 @@ class Collector(Process):
         super().__init__()
         self.daemon = True
 
+    def _exchange_feed_options(self, feed_config: dict):
+        """
+        Parses feed config values from config.yaml.
+        Exchange specific options will be derived from keyword args of the exchange FeedHandler class.
+
+        ex: 'depth_interval' option will be consumed from config since it is found in cryptofeed.exchange.Binance.__init__
+        """
+        # base Feed options
+        base_feed_options = {'book_interval', 'max_depth', 'snapshot_interval'}
+
+        # exchange specific Feed options
+        ExchangeFeedClass = EXCHANGE_MAP[self.exchange]
+        exchange_feed_options = inspect.signature(ExchangeFeedClass.__init__).parameters
+        exchange_feed_options = {key for key in exchange_feed_options if key not in {'self', '**kwargs'}}
+
+        available_options = base_feed_options.union(exchange_feed_options)
+
+        fh_kwargs = {'book_interval': 1000}
+        for key, value in feed_config.items():
+            if key in available_options:
+                fh_kwargs[key] = value
+
+        return fh_kwargs
+
     def run(self):
         LOG.info("Collector for %s running on PID %d", self.exchange, os.getpid())
-
         cache = self.config['cache']
         retries = self.exchange_config.pop('retries', 30)
         timeouts = self.exchange_config.pop('channel_timeouts', {})
         fh = FeedHandler()
 
-        for callback_type, value in self.exchange_config.items():
+        for callback_type, feed_config in self.exchange_config.items():
+            # config value can be a dict or list of symbols
+            feed_kwargs = {'retries': retries, 'timeout': timeouts.get(callback_type, 120)}
+            if isinstance(feed_config, dict):
+                feed_kwargs.update(self._exchange_feed_options(feed_config))
+                book_delta = feed_config.get('book_delta', False)
+            else:
+                book_delta = False
+
             cb = {}
-            delta = False
-            fh_kwargs = {'timeout': timeouts.get(callback_type, 120), 'book_interval': 1000}
-
-            if 'book_interval' in value:
-                fh_kwargs['book_interval'] = value['book_interval']
-            if 'book_delta' in value and value['book_delta']:
-                delta = True
-            if 'max_depth' in value:
-                fh_kwargs['max_depth'] = value['max_depth']
-            if 'snapshot_interval' in value:
-                fh_kwargs['snapshot_interval'] = value['snapshot_interval']
-
             if callback_type in (L2_BOOK, L3_BOOK):
                 self.exchange_config[callback_type] = self.exchange_config[callback_type]['symbols']
 
@@ -56,7 +76,7 @@ class Collector(Process):
                 from cryptofeed.backends.redis import TradeStream, BookStream, BookDeltaStream, TickerStream, FundingStream, OpenInterestStream, LiquidationsStream, MarketInfoStream, CandlesStream
                 trade_cb = TradeStream
                 book_cb = BookStream
-                book_up = BookDeltaStream if delta else None
+                book_up = BookDeltaStream if book_delta else None
                 market_info_cb = MarketInfoStream
                 ticker_cb = TickerStream
                 funding_cb = FundingStream
@@ -67,7 +87,7 @@ class Collector(Process):
                 from cryptofeed.backends.kafka import TradeKafka, BookKafka, BookDeltaKafka, TickerKafka, FundingKafka, OpenInterestKafka, LiquidationsKafka, MarketInfoKafka, CandlesKafka
                 trade_cb = TradeKafka
                 book_cb = BookKafka
-                book_up = BookDeltaKafka if delta else None
+                book_up = BookDeltaKafka if book_delta else None
                 market_info_cb = MarketInfoKafka
                 ticker_cb = TickerKafka
                 funding_cb = FundingKafka
@@ -115,8 +135,6 @@ class Collector(Process):
                         cb[L2_BOOK].append(BookZMQ(host=host, port=port))
                     elif callback_type == L3_BOOK:
                         cb[L3_BOOK].append(BookZMQ(host=host, port=port))
-                    elif callback_type == MARKET_INFO:
-                        cb[MARKET_INFO].append(MarketInfoZMQ(host=host, port=port))
                     elif callback_type == OPEN_INTEREST:
                         cb[OPEN_INTEREST].append(OpenInterestZMQ(host=host, port=port))
                     elif callback_type == TICKER:
@@ -126,6 +144,7 @@ class Collector(Process):
                     if BOOK_DELTA in cb:
                         cb[BOOK_DELTA].append(BookDeltaZMQ(host=host, port=port))
 
-            fh.add_feed(self.exchange, retries=retries, subscription={callback_type: self.exchange_config[callback_type]}, callbacks=cb, **fh_kwargs)
+            fh.add_feed(self.exchange, subscription={callback_type: self.exchange_config[callback_type]}, callbacks=cb, **feed_kwargs)
+            LOG.info(f"Collector added feed handler - {self.exchange}({callback_type.upper()}, {({'book_delta': book_delta, **feed_kwargs})})")
 
         fh.run()
